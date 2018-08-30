10a8f9f679edac97eea5d32ab6a11084d238d3cf
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2018-08-30 17:11:46 -0500

    Kernel updates and playfield testing.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd src
-dasm spider.asm -DNTSC=1 -DCOLOR_NTSC=1 -f3 -lspider.lst -ospider.bin
+dasm spider.asm -DNTSC=1 -DCOLOR_NTSC=1 -f3 -v5 -lspider.lst -ospider.bin
 mv ./spider.lst ../bin/spider.lst
 mv ./spider.bin ../bin/spider.bin
 cd ../
--- src/spider.asm
+++ src/spider.asm
@@ -7,10 +7,43 @@
     include "vcs.h"
     include "macro.h"
 
+; Constants
+PATTERN         = $80   ; storage location (1st byte in RAM)
+TIMETOCHANGE    = 20    ; speed of animation
+
     SEG
     ORG $F000
 
 Reset
+
+InitializeStack
+
+    ldx #0
+    txa
+
+InitializeStackLoop
+
+    dex
+    txs
+    pha
+    bne InitializeStackLoop
+
+    ; Stack pointer now $FF, a=x=0, TIA registers (0 - $7F) = RAM ($80 - $FF) = 0
+
+    ; Set background color
+    lda #0
+    sta COLUBK
+
+    ; The binary PF pattern
+    lda #0
+    sta PATTERN
+
+    ; Set the playfield color
+    lda #$45
+    sta COLUPF
+
+    ; "speed" counter
+    ldy #0
 
 StartOfFrame
 
@@ -33,93 +66,66 @@ StartOfFrame
 
     ; 37 scanlines of vertical blank
 
+    ldx #0
+VerticalBlank
     sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
+    inx
+    cpx #37
+    bne VerticalBlank
 
-    ; 192 scanlines of picture
+    ; Handle a change in the pattern once every 20 frames
+    ; and write the pattern to the PF1 register
+    iny                 ; Increment speed count by one
+    cpy #TIMETOCHANGE   ; has it reached our "change point"?
+    bne notyet          ; no, so branch past
+
+    ldy #0              ; reset speed count
+
+    inc PATTERN         ; switch to next pattern
+
+notyet
+
+    lda PATTERN         ; use our saved pattern
+    sta PF1             ; as the playfield shape
+
+    ; Do 192 scanlines of color-changing (our picture)
+
+Picture
+
+    ldx #0              ; This counts our scanline number
+
+PictureLoop
+
+    sta WSYNC
+    inx
+    cpx #192
+    bne PictureLoop
+
+    ;----------------------------------------
+
+    lda #%01000010
+    sta VBLANK          ; end of screen - enter blanking
+
+; 30 scanlines of overscan...
+
+Overscan
 
     ldx #0
 
-    REPEAT 192          ; scanlines
-        inx
-        stx COLUBK
-        sta WSYNC
-    REPEND
-
-    lda #%01000010
-    sta VBLANK          ; end of screen -enter blanking
-
-    ; 30 scanlines of overscan
+OverscanLoop
 
     sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
+    inx
+    cpx #30
+    bne OverscanLoop
 
     jmp StartOfFrame
 
+    ;-------------------------------------------
+
     ORG $FFFA
+
+InterruptVectors
 
     .word Reset         ; NMI
     .word Reset         ; RESET
