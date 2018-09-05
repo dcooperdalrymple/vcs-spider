e750632ba2bcb742ad91dce03adf35b374919462
dcooperdalrymple <dcooperdalrymple@gmail.com>
2018-09-05 00:17:15 -0500

    Image tests.

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
+cd src
 dasm spider.asm -DNTSC=1 -DCOLOR_NTSC=1 -f3 -lspider.lst -ospider.bin
-copy spider.lst bin\spider.lst
-copy spider.bin bin\spider.bin
-del spider.lst
-del spider.bin
+copy .\spider.lst ..\bin\spider.lst
+copy .\spider.bin ..\bin\spider.bin
+del .\spider.lst
+del .\spider.bin
+cd ..\
--- src/spider.asm
+++ src/spider.asm
@@ -7,21 +7,60 @@
     include "vcs.h"
     include "macro.h"
 
+;================
 ; Constants
-PATTERN         = $80   ; storage location (1st byte in RAM)
-TIMETOCHANGE    = 20    ; speed of animation
+;================
+
+; PAL Region
+PAL                 = 0
+PAL_SCANLINES       = 242
+PAL_TOTAL           = 312
+
+; NTSC Region
+NTSC                = 1
+NTSC_SCANLINES      = 192
+NTSC_TOTAL          = 262
+
+; Kernel
+SYSTEM              = NTSC
+#if SYSTEM = NTSC
+KERNEL_SCANLINES    = NTSC_SCANLINES
+KERNEL_TOTAL        = NTSC_TOTAL
+#endif
+#if SYSTEM = PAL
+KERNEL_SCANLINES    = PAL_SCANLINES
+KERNEL_TOTAL        = PAL_TOTAL
+#endif
+KERNEL_VSYNC        = 3
+KERNEL_VBLANK       = 37
+KERNEL_OVERSCAN     = 30
+
+; Logo
+LOGO_START          = 32
+LOGO_HEIGHT         = 48
+LOGO_END            = 80
+
+;================
+; Variables
+;================
+
+    SEG.U vars
+    org $80
+
+logoDrawLine        ds 1
 
     SEG
-    ORG $F000
 
-Reset
+    ORG $F000           ; Start of cart area
 
-InitializeStack
+Reset:
+
+InitializeStack:
 
     ldx #0
     txa
 
-InitializeStackLoop
+InitializeStackLoop:
 
     dex
     txs
@@ -30,10 +69,10 @@ InitializeStackLoop
 
     ; Stack pointer now $FF, a=x=0, TIA registers (0 - $7F) = RAM ($80 - $FF) = 0
 
-InitializeVariables
+InitializeVariables:
 
     ; Set background color
-    lda #0
+    lda #$00 ; White
     sta COLUBK
 
     ; Set the playfield color
@@ -44,9 +83,21 @@ InitializeVariables
     lda #%00000001
     sta CTRLPF
 
-StartOfFrame
+    ; Disable Game Elements
+    lda #$00
+    sta ENABL           ; Turn off ball
+    sta ENAM0           ; Turn off player 1 missile
+    sta ENAM1           ; Turn off player 2 missile
+    sta GRP0            ; Turn off player 1
+    sta GRP1            ; Turn off player 2
 
-    ; Start of vertical blank processing
+    ; Empty playfield
+    lda #%00000000
+    sta PF0
+    sta PF1
+    sta PF2
+
+StartOfFrame:           ; Start of vertical blank processing
 
     lda #0
     sta VBLANK
@@ -54,92 +105,104 @@ StartOfFrame
     lda #2
     sta VSYNC
 
-    ; 3 scanlines of VSYNCH signal
-
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
+    ; VSYNCH signal scanlines
+    REPEAT #KERNEL_VSYNC
+        sta WSYNC
+    REPEND
 
     lda #0
     sta VSYNC
 
-    ; 37 scanlines of vertical blank
-
-VerticalBlank
+VerticalBlank:          ; scanlines of vertical blank
 
     ldx #0
 
-VerticalBlankLoop
+VerticalBlankLoop:
 
     sta WSYNC
     inx
-    cpx #37
+    cpx #KERNEL_VBLANK
     bne VerticalBlankLoop
 
-    ; Do 192 scanlines of color-changing (our picture)
-
-Picture
+Picture:                ; Do 192 scanlines
 
     ldx #0              ; This counts our scanline number
+    ldy #$FF              ; This counts our logo line number, set to $FF to loop
 
-SolidPattern
+PictureScanline:
 
-    lda #%11111111
-    sta PF0
+    ; Check if we are outside of logo bounds
+    cpx #LOGO_START
+    bcc PictureScanlineClear
+    cpx #LOGO_END
+    bcs PictureScanlineClear
+
+    ; Increment logo line and check for 4th bit change (every 8)
+    tya
+    iny
+    sty $80
+    eor $80
+    and #%00001000
+    beq PictureScanlineEnd
+
+    tya                 ; Load up logo index
+    pha                 ; Push logo line index to stack
+    lsr                 ; Divide by 8 (3 bitshifts)
+    lsr
+    lsr
+    tay                 ; Move accumulator to y
+    lda LogoData,y      ; Load logo data from yth line
+    sta PF1             ; Store logo line into playfield
+    pla                 ; Recall logo line index from stack
+    tay                 ; Store logo line index back into y register
+    jmp PictureScanlineEnd
+
+PictureScanlineClear:
+
+    lda #0
     sta PF1
-    sta PF2
-    jmp PictureLoop
 
-WallPattern
-
-    lda #%00010000
-    sta PF0
-    lda #%00000000
-    sta PF1
-    sta PF2
-
-PictureLoop
+PictureScanlineEnd:
 
     sta WSYNC
     inx
 
-    ; Pattern Changes
-    cpx #8
-    beq WallPattern
-    cpx #184
-    beq SolidPattern
+    cpx #KERNEL_SCANLINES
+    bne PictureScanline
 
-    cpx #192
-    bne PictureLoop
-
-    ;----------------------------------------
+Overscan:               ; 30 scanlines of overscan...
 
     lda #%01000010
     sta VBLANK          ; end of screen - enter blanking
 
-; 30 scanlines of overscan...
-
-Overscan
-
     ldx #0
 
-OverscanLoop
+OverscanLoop:
 
     sta WSYNC
     inx
-    cpx #30
+    cpx #KERNEL_OVERSCAN
     bne OverscanLoop
 
     jmp StartOfFrame
 
+LogoData:               ; 6 bytes over 8 lines each, total of 48 lines
+
+    .BYTE %00001000
+    .BYTE %00011100
+    .BYTE %00110110
+    .BYTE %01100000
+    .BYTE %11000000
+    .BYTE %11111111
+
     ;-------------------------------------------
 
-    ORG $FFFA
+    ORG $FFFA           ; End of cart area
 
-InterruptVectors
+InterruptVectors:
 
     .word Reset         ; NMI
     .word Reset         ; RESET
     .word Reset         ; IRQ
 
-END
+    END
