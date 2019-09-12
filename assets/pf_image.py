5314b223cf58122cf22127175c00ecad1af2596d
dcooperdalrymple <dcooperdalrymple@gmail.com>
2019-09-12 17:05:17 -0500

    Assets management and assembly generation scripts.

---
+++ assets/pf_image.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python2.7
+
+# Initialize the program and modules
+
+try:
+    import sys
+    from PIL import Image
+    import math
+
+except ImportError, err:
+    print "Could not load %s module." % (err)
+    raise SystemExit
+
+print "\nVCS Playfield Image Converter\nCopyright (c) 2019 D Cooper Dalrymple\n"
+
+# Constants
+PF_HEIGHT = 192 / 8
+PF_MIRROR_WIDTH = 4 + 8 + 8
+PF_FULL_WIDTH = PF_MIRROR_WIDTH * 2
+OUTPUT_TAB_LENGTH = 4
+
+def load_image(filename):
+    image = Image.open(filename)
+    print("Image successfully loaded.")
+    print("Width: " + str(image.width) + "px")
+    print("Height: " + str(image.height) + "px\n")
+    return image
+
+def read_image(image):
+    data = [[0 for x in range(0, PF_FULL_WIDTH)] for y in range(0, PF_HEIGHT)]
+
+    for y in range(0, min(image.height, PF_HEIGHT)):
+        for x in range(0, min(image.width, PF_FULL_WIDTH)):
+            pixel = image.getpixel((x, y))
+            avg = sum(pixel) / float(len(pixel))
+            if (avg >= 255 / 2.0):
+                data[y][x] = 1
+
+    return data
+
+def set_bit(v, index, x): # Index is LSB
+    mask = 1 << index
+    v &= ~mask
+    if x:
+        v |= mask
+    return v
+
+def get_bin(n):
+    b = ""
+    i = 0
+    while i < 8:
+        b += str(int(n % 2))
+        n /= 2
+        i += 1
+    return b[::-1]
+
+def convert_mirror(image_data):
+    pf_data = [0b00000000 for x in range(0, PF_HEIGHT * 3)]
+
+    i = 0 # pf_data index
+    for y in range(0, PF_HEIGHT):
+        j = 0 # image_data x index
+
+        for x in range(0, 4):
+            # First 4 bits reversed (MSB)
+            pf_data[i] = set_bit(pf_data[i], x + 4, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Normal 8 bits
+            pf_data[i] = set_bit(pf_data[i], 7 - x, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Reversed 8 bits
+            pf_data[i] = set_bit(pf_data[i], x, image_data[y][j])
+            j += 1
+        i += 1
+
+        # Leave second half of image untouched
+
+    return pf_data
+
+def convert_full(image_data):
+    pf_data = [0b00000000 for x in range(0, PF_HEIGHT * 3 * 2)]
+
+    i = 0 # pf_data index
+    for y in range(0, PF_HEIGHT):
+        j = 0 # image_data x index
+
+        for x in range(0, 4):
+            # First 4 bits reversed (LSB)
+            pf_data[i] = set_bit(pf_data[i], x + 4, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Normal 8 bits
+            pf_data[i] = set_bit(pf_data[i], 7 - x, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Reversed 8 bits
+            pf_data[i] = set_bit(pf_data[i], x, image_data[y][j])
+            j += 1
+        i += 1
+
+        # Just doubles it for second half of image
+        for x in range(0, 4):
+            # First 4 bits reversed (LSB)
+            pf_data[i] = set_bit(pf_data[i], x + 4, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Normal 8 bits
+            pf_data[i] = set_bit(pf_data[i], 7 - x, image_data[y][j])
+            j += 1
+        i += 1
+
+        for x in range(0, 8):
+            # Reversed 8 bits
+            pf_data[i] = set_bit(pf_data[i], x, image_data[y][j])
+            j += 1
+        i += 1
+
+    return pf_data
+
+def reverse_data(data, group_size):
+    rev_data = [0 for x in range(0, len(data))]
+
+    groups = int(math.floor(len(data) / float(group_size)))
+    for x in range(0, groups):
+        for y in range(0, group_size):
+            rev_data[(groups - x - 1) * group_size + y] = data[x * group_size + y]
+
+    return rev_data
+
+def compose_output(data, group_size, address_name):
+    output = address_name + ":\n"
+
+    groups = int(math.floor(len(data) / float(group_size)))
+    for x in range(0, groups):
+        output += "\n"
+        for y in range(0, group_size):
+            output += " " * OUTPUT_TAB_LENGTH + ".BYTE %" + get_bin(data[x * group_size + y]) + "\n"
+
+    return output
+
+# Parse command line arguments
+
+# No input, prompt user
+if len(sys.argv) < 2:
+    print "No arguments given. Run with -h for a list of options."
+    raise SystemExit
+
+# Help message
+elif sys.argv[1] == "-h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+    print "Convert mirrored monochromatic image to playfield bytes:"
+    print "  pf_image.py -type mirror -name [ADDRESSNAME] -in [FILENAME] -out [FILENAME]"
+    print "Convert fullscreen monochromatic image to playfield bytes:"
+    print "  pf_image.py -type full -name [ADDRESSNAME] -in [FILENAME] -out [FILENAME]"
+    print "\nAccepts png/jpg input and .asm output is preferred."
+    raise SystemExit
+
+if len(sys.argv) == 9 and sys.argv[1] == '-type' and sys.argv[3] == '-name' and sys.argv[5] == '-in' and sys.argv[7] == '-out':
+    type = sys.argv[2]
+    address_name = sys.argv[4]
+    in_file = sys.argv[6]
+    out_file = sys.argv[8]
+
+    image = load_image(in_file)
+    image_data = read_image(image)
+
+    if type == 'mirror':
+        pf_data = convert_mirror(image_data)
+        group_size = 3
+    elif type == 'full':
+        pf_data = convert_full(image_data)
+        group_size = 6
+    else:
+        print "Invalid conversion type. Must be \"mirror\" or \"full\"."
+        raise SystemExit
+
+    pf_data = reverse_data(pf_data, group_size)
+    pf_output = compose_output(pf_data, group_size, address_name)
+
+    output = open(out_file, "w")
+    output.write(pf_output)
+    output.close()
+
+    print "Successfully converted image to playfield data.\n"
+    raise SystemExit
+
+print "Invalid arguments. Run with -h for a list of options."
---
+++ assets/sprite_image.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python2.7
+
+# Initialize the program and modules
+
+try:
+    import sys
+    from PIL import Image
+    import math
+
+except ImportError, err:
+    print "Could not load %s module." % (err)
+    raise SystemExit
+
+print "\nVCS Sprite Image Converter\nCopyright (c) 2019 D Cooper Dalrymple\n"
+
+# Constants
+SPRITE_WIDTH = 8
+OUTPUT_TAB_LENGTH = 4
+
+def load_image(filename):
+    image = Image.open(filename)
+    print("Image successfully loaded.")
+    print("Width: " + str(image.width) + "px")
+    print("Height: " + str(image.height) + "px")
+    print("Frames: " + str(int(math.floor(image.width / float(SPRITE_WIDTH)))) + "\n")
+    return image
+
+def read_image(image):
+    frames = int(math.floor(image.width / float(SPRITE_WIDTH)))
+    data = [[[0 for x in range(0, SPRITE_WIDTH)] for y in range(0, image.height)] for i in range(0, frames)]
+
+    for i in range(0, frames):
+        for y in range(0, image.height):
+            for x in range(0, SPRITE_WIDTH):
+                pixel = image.getpixel((x + SPRITE_WIDTH * i, y))
+                avg = sum(pixel) / float(len(pixel))
+                if (avg >= 255 / 2.0):
+                    data[i][y][x] = 1
+
+    return data
+
+def set_bit(v, index, x): # Index is LSB
+    mask = 1 << index
+    v &= ~mask
+    if x:
+        v |= mask
+    return v
+
+def get_bin(n):
+    b = ""
+    i = 0
+    while i < 8:
+        b += str(int(n % 2))
+        n /= 2
+        i += 1
+    return b[::-1]
+
+def convert_bin(image_data):
+    sprite_data = [[0b00000000 for y in range(0, len(image_data[0]))] for i in range(0, len(image_data))]
+
+    for i in range(0, len(image_data)):
+        for y in range(0, len(image_data[i])):
+            for x in range(0, SPRITE_WIDTH):
+                sprite_data[i][y] = set_bit(sprite_data[i][y], (SPRITE_WIDTH - 1) - x, image_data[i][y][x])
+
+    return sprite_data
+
+def reverse_data(sprite_data):
+    rev_data = [[0b00000000 for y in range(0, len(sprite_data[0]))] for i in range(0, len(sprite_data))]
+
+    for i in range(0, len(sprite_data)):
+        for y in range(0, len(sprite_data[0])):
+            rev_data[i][len(sprite_data[0]) - 1 - y] = sprite_data[i][y]
+
+    return rev_data
+
+def compose_output(sprite_data, address_name):
+    output = address_name + ":\n"
+
+    for i in range(0, len(sprite_data)):
+        output += "\n"
+        for y in range(len(sprite_data[0])):
+            output += " " * OUTPUT_TAB_LENGTH + ".BYTE %" + get_bin(sprite_data[i][y]) + "\n"
+
+    return output
+
+# Parse command line arguments
+
+# No input, prompt user
+if len(sys.argv) < 2:
+    print "No arguments given. Run with -h for a list of options."
+    raise SystemExit
+
+# Help message
+elif sys.argv[1] == "-h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+    print "Convert " + str(SPRITE_WIDTH) + " pixel wide sprite frames to GRP bytes:"
+    print "  sprite_image.py -name [ADDRESSNAME] -in [FILENAME] -out [FILENAME]"
+    print "\nAccepts png/jpg input and .asm output is preferred."
+    raise SystemExit
+
+if len(sys.argv) == 7 and sys.argv[1] == '-name' and sys.argv[3] == '-in' and sys.argv[5] == '-out':
+    address_name = sys.argv[2]
+    in_file = sys.argv[4]
+    out_file = sys.argv[6]
+
+    image = load_image(in_file)
+    image_data = read_image(image)
+
+    sprite_data = convert_bin(image_data)
+    sprite_data = reverse_data(sprite_data)
+    sprite_output = compose_output(sprite_data, address_name)
+
+    output = open(out_file, "w")
+    output.write(sprite_output)
+    output.close()
+
+    print "Successfully converted image to sprite frames.\n"
+    raise SystemExit
+
+print "Invalid arguments. Run with -h for a list of options."
