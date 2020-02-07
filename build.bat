976e25c1e13efbae85595eacd90cc9c183ed81e8
dcooperdalrymple <dcooperdalrymple@gmail.com>
2020-02-06 18:10:27 -0600

    Removed old windows build file. It's preferred to build using build.sh on bash emulator (ie Git Bash).

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd src
-dasm kernel.asm -DNTSC=1 -DCOLOR_NTSC=1 -f3 -lrom.lst -orom.bin
-copy .\rom.lst ..\bin\rom.lst
-copy .\rom.bin ..\bin\rom.bin
-del .\rom.lst
-del .\rom.bin
-cd ..\
